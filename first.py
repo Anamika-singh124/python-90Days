48359995f5a70f5cea0877efb71d48e74b736568
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-05 16:22:33 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,32 @@
+name = "anamika"
+age = "16"
+price = 26.6
+print(name)
+print(age)
+print(price) 
+a = 23
+b = 3
+print(a+b)  
+text = "Hello" + "world"
+print(text)
+str = "anamika"
+print(str[4])
+str = "anamika"
+print(str[2:5])
+text = "anamika"
+print(len(text))
+name = "coder"
+age = "16"
+print("my name is :",name)
+print("my age is :",age) 
+a = 29
+b= 23
+print(a-b)
+text = "anshika"+ "soun"
+print(text)
+name = "nivedita"
+print(name[5])
+print(name[0:6])
+print(name[-6:-3])
+name = "anamika"
+print(name[-4:-1])
