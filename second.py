51e174f80693189d68f2c3ae27971c207e18e59d
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-06 21:29:18 +0530

    string function,grade ,looping  ,length of string
    day2 : practiced string function,grade program,looping through string,length of string

---
+++ second.py
@@ -0,0 +1,34 @@
+str = "i am coder"
+print(str.endswith("i am coder"))
+print(str.capitalize())
+print(str.replace("coder" ," good "))
+print(str.find("a"))
+print(str.count("coder"))
+marks = 72
+if marks >= 90: 
+    grade ="A"
+elif marks >= 80:
+    grade = "B"
+elif marks >= 70:
+      grade = "C" 
+else:     
+     grade = "D"
+print("Grade:",grade)
+a = """i am 
+anamika singh"""
+print(a)
+name = "golu"
+print(name[0])
+print(name[3])
+for ch in name:
+     print(ch)
+sport = "hockey"
+len1 = len(sport)
+print(len1)
+print(sport[:3])
+print(sport[0])
+alp = "AbCdE"
+for i in alp:
+     print(i)
+
+
