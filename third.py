16164ed11a295407bf4e97a439e780f7c8d44744
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-07 18:28:13 +0530

    string methods
    upper ,lower  strip ,rstrip,  split, capitalize ,center ,count ,endswith,find,index.isalnum,isalpha,islower.isspace,istitle,isupper practice topices

---
+++ third.py
@@ -0,0 +1,42 @@
+str = "i am jack"
+print(str.upper())
+str = "I AM DAN"
+print(str.lower())
+str = "big   small"
+print(str.strip())
+str = "BYE!!!!!"
+print(str.rstrip("!"))
+str = "think and grow"
+print(str.split("  "))
+text = "i am done"
+print(text.capitalize())
+str = "welcome the family"
+print(str.center(20))
+text = "Hello"
+print(text.center( 5,"#"))
+str = "get out you"
+print(str.count("out"))
+str = "i learn python!!!!"
+print(str.endswith("!!!!"))
+print(text.find("arn"))
+print(text.replace("python","java"))
+str = "i am good .are you good."
+print(str.find("are"))
+str =  "welcome to my home"
+print(str.isalnum())
+str = "welcome"
+print(str.isalpha())
+str = "hardwork"
+print(str.islower())
+str = "        "
+str = "        "
+print(str.isspace())
+print(str.isspace())
+str = "World Health Organization"
+print(str.istitle())
+str = "hello"
+print(str.istitle())
+str = "he's name is dan .dan is an honest man."
+print(str.title())
+str = "python is a compiled language"
+print(str.swapcase())
