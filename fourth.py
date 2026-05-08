bf2a21c68e9e59b48feb52dc5b8b48bbe9d224ff
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-08 20:00:46 +0530

    if-else ,nested -if statements

---
+++ fourth.py
@@ -0,0 +1,36 @@
+apple_price = 210
+budget = 200 
+if apple_price <= budget:
+    print("Alexa, add 1kg apples to cart")
+else:
+    print("alexa,do not add apples")
+num = 10
+if num > 0:
+    if num % 2 == 0:
+        print("positive even")
+num = 0
+if num > 0:
+    print("positive")
+elif num < 0:
+    print("negative")
+else:
+    print("zero")
+num = 0
+if num > 0:
+    print("positive")
+elif num == 0:
+    print("zero")
+else:
+    print("negative")
+num = 18
+if (num < 0):
+    print("number is negative")
+elif (num > 0):
+    if (num <= 10):
+        print("number is between 1-10")
+elif (num > 10 and num <= 20):
+    print("number is between 11-20")
+else:
+    print("number is greater than 20 ")
+print("number is between 11-20")
+
