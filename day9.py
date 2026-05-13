d0cf37cadca0f96168c1e2a7e2a81ae571dc95ba
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-13 14:29:06 +0530

    f-string in python,docstrings in python

---
+++ day9.py
@@ -0,0 +1,31 @@
+name = "Anamika"
+age = 18
+print(f"my name is {name} and age is {age}")
+def add (a,b):
+    """this function adds two numbers"""
+    return a+b
+print(add(2,3))
+print(add.__doc__)
+#print("hello")
+def square(n):
+    """return square of number"""
+    return n*n
+print(square.__doc__)
+def greet():
+    """this function prints hello"""
+    print("hello")
+def square(n):
+    """returns square"""
+    return n*n
+print(square. __doc__ )
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)   
+print(factorial(5))
+5*factorial(4)
+def add(a,b):
+    """this function adds two numbers"""
+    return a+b
+print(add.__doc__)
+print(add.__name__)
