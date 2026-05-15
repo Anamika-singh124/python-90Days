0f371388ed3fbd95c2c541d486adfa353623acbf
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-15 16:06:48 +0530

    string

---
+++ Revision#1.py
@@ -0,0 +1,41 @@
+name = "Anamika"
+age = 18
+print(name)
+print(age)
+a = 23
+b = 34
+print(a+b)
+a = "hello"
+b = "world"
+print(a+b)
+text = "anamika" 
+print(len(text))
+print(text[3])
+print(text[6])
+print(text[3:6])
+print(text[-4:-1])
+print(text[-7:-4])
+str = "i am anamika."
+print(str.endswith("anamika."))
+print(str.capitalize())
+print(str.find(a))
+print(str.count("am"))
+print(str.replace("anamika","golu"))
+name = "nisha"
+for i in name:
+    print(name)
+friend = "nisha"
+len1 = len(friend)
+print(len1)
+for i in friend:
+    print(i)
+str = "abcde"
+print(str.upper())
+str = "GHBCYJT"
+print(str.lower())
+str = "silver spoon"
+print(str.strip())
+str  = "hello!!!"
+print(str.rstrip("!"))
+str = "anamika singh"
+print(str.split("   "))
