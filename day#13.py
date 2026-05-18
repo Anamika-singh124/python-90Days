4ed6054e1d43922135a260069372c4e4f6cff354
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-18 15:17:35 +0530

    virtual environment

---
+++ day#13.py
@@ -0,0 +1,16 @@
+import math 
+result = math.sqrt(9)
+print(result)
+from math import sqrt
+result = sqrt(9)
+print(result)
+from math import pi
+print(pi)
+from math import *
+result = sqrt(9)
+print(result)
+print(pi)
+import math as m
+result = m.sqrt(9)
+print(result)
+print(m.pi)
