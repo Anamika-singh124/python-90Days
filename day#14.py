a37b445e6ca73495f723e39b9292635ca39d38f7
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-19 11:29:41 +0530

    dir function, def main

---
+++ day#14.py
@@ -0,0 +1,31 @@
+x = 10
+print(dir())
+name = "python"
+print(dir(name))
+nums = [1,2,3]
+print(dir(nums))
+import math
+print(dir(math))
+name = "anamika"
+print(dir(name))
+import math
+print(dir(math))
+def main():
+    print("hello python")
+
+main()
+def main():
+    name = input("enter name:")
+    print("hello",name)
+main()
+def add():
+    print(10 + 20)
+def main():
+    print("program strt")
+    add()
+main()
+def main():
+    print("python")
+    if __name__ == "__main__":
+        main()
+        
