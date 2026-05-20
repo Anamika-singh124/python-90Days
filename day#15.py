5a969e35d3d5a6cc8d5ccb228263227e0e2cbe47
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-20 16:06:43 +0530

    os  module in python

---
+++ day#15.py
@@ -0,0 +1,40 @@
+import os
+print(os.listdir())
+import os
+print(os.getcwd())
+import os
+print(os.listdir())
+import subprocess
+subprocess.run(["dir"], shell = True)
+import subprocess
+subprocess.run("notepad", shell = True)
+import os
+os.system("dir")
+import os 
+os.system("notepad")
+def test():
+    x = 10
+    print(x)
+test()
+x = 100
+def test():
+    print(x)
+test()
+x = 10
+def test():
+    global x
+    x = 50
+
+test()
+print(x)
+x = 5
+def demo():
+    print(x)
+demo()
+def hello():
+    name = "Anamika"
+    print(name)
+hello()
+
+
+
