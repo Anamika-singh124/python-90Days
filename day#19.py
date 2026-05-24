97f45fa35ce670f93b32d2c91126ec33e213eb07
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-24 14:24:45 +0530

    self parameter,constructors in python

---
+++ day#19.py
@@ -0,0 +1,66 @@
+class Student:
+    def hello(self):
+        print("hello")
+s1 = Student()
+s1.hello()
+class Student:
+    def __init__(self,name):
+        self.name = name
+s1 = Student("Anamika")
+print(s1.name)
+class Car:
+    def __init__(self,brand):
+        self.brand = brand
+    def show(self):
+        print(self.brand)
+c1 = Car("Bmw")
+c1.show()
+class Student:
+    def __init__(self,name):
+        self.name = name
+s1 = Student("Anamika")
+print(s1.name)
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self .age = age
+s1 = Student("Anamika",18)
+print(s1.name)
+print(s1.age)
+class Car:
+    def __init__(self,brand):
+        self.brand = brand
+    def show(self):
+        print(self.brand)
+c1 = Car("BMW")
+c1.show()
+class Student:
+    def __init__(self,name):
+        self.name = name
+s1 = Student("Anamika")
+print(s1.name)
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+s1 = Student("Anamika",18)
+print(s1.name)
+print(s1.age)
+class Mobile:
+    def __init__(self,brand):
+        self.brand = brand
+m1 = Mobile("Samsung")
+print(m1.brand)
+class Car:
+    def __init__(self):
+        self.brand = "BMw"
+c1 = Car()
+print(c1.brand)
+class Mobile:
+    def __init__(self):
+        print("Mobile Object Created")
+m1 = Mobile()
+
+
+
+
