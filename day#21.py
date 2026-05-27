cc67893bae2e246edcab413c8cef2974a6a671e8
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-05-27 17:20:32 +0530

    Instance VS class variables

---
+++ day#21.py
@@ -0,0 +1,43 @@
+class Student:
+    def __init__(self):
+        self ._name = "Anamika"
+s1 = Student()
+print(s1._name)
+class Student:
+    def __init__(self):
+        self._name = "Anamika"
+s1 = Student()
+print(s1._name)
+class Parent:
+    def __init__(self):
+        self ._money = 5000
+class Child(Parent):
+    def show(self):
+        print(self ._money)
+c1 = Child()
+c1.show()
+class Student:
+    college = "lpu"
+s1 = Student()
+s2 = Student()
+print(s1.college)
+print(s2.college)
+class  Student:
+    def __init__(self,name):
+        self .name = name
+s1 = Student("Anamika")
+s2 = Student("anshika")
+print(s1.name)
+print(s2.name)
+class Student:
+    college = "lpu"
+    def __init__(self,name):
+        self.name = name
+s1 = Student("Anamika")
+s2 = Student("anshika")
+print(s1.name)
+print(s1.college)
+print(s2.name)
+print(s2.college)
+
+
