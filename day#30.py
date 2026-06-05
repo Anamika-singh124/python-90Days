58d856429b2f650efec0ec8ed10f256bb57818ae
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-06-05 16:33:51 +0530

    The time Module in python

---
+++ day#30.py
@@ -0,0 +1,28 @@
+import time
+print(time.time())
+import time 
+print("Start")
+time.sleep(3)
+print("end")
+import time
+print(time.ctime())
+import time 
+
+print("A")
+time.sleep(2)
+print("B")
+
+import time
+
+current_time = time.strftime("%H: %M: %S")
+print(current_time)
+
+import time
+print (time.strftime("%d/%m/%Y"))
+
+import time
+
+print(time.strftime("%d-%m-%Y %H:%M: %S"))
+
+import time
+print(time.strftime("%Y"))
