ff213f790a785a6940c8e2938d31b5c3b4880825
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-06-07 12:54:17 +0530

    Walrus operator in python

---
+++ day#32.py
@@ -0,0 +1,13 @@
+print(name := input("Enter your name:"))
+if(n :=len("Anamika")) > 5:
+    print("Length =",n)
+
+
+numbers = [10,20,30]    
+if (count := len(numbers))  == 3:
+    print("count =",count)
+
+if(x := 15) >10:
+    print(x)
+
+
