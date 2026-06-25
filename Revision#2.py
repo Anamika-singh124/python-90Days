23c3e214ced2eb9776a1153df60c99e490462cc3
Anamika-singh124 <yanamika9936singh@gmail.com>
2026-06-25 12:53:02 +0530

    if- else statements

---
+++ Revision#2.py
@@ -0,0 +1,45 @@
+nums = 25
+if  nums % 2 == 0:
+    print("even")
+else: 
+    print("odd")
+nums = -23
+if nums > 0:
+    print("positive")
+else:
+    print("negative")
+nums = 18   
+if nums <=  10:
+    print("number is betweeen 1-10")
+elif nums <= 20:
+    print("number is greater then 11-20")
+else:
+    print("number is zero")
+age = 20
+if age > 18:
+    print("yes")
+else: 
+    print("no")
+marks =  20
+if marks >= 33:
+    print("pass")
+else: 
+    print("fail")
+grade = 80
+if grade >= 90:
+    print("pass")
+elif grade >= 75:
+    print("pass")
+elif grade >= 50:
+    print("fail")
+else:
+    print( "fail")
+character = ("a,e,i,o,u")
+for i in character:
+    print(i)
+
+
+
+
+
+ 
